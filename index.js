a33f5bb5d9ac0be20425d0d3453d288abdb8045c
Robin Berjon <robin@berjon.com>
2016-05-24 13:06:57 -0400

    tests

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ var fs = require('fs')
   , once = require('once')
   , devConfig = join(__dirname, 'dev.json')
   , prodConfig = join(__dirname, 'prod.json')
+  , postcssExec = join(__dirname, 'node_modules/.bin/postcss')
 ;
 
 module.exports = function (options, cb) {
-  let args = [];
+  var args = [];
   if (options.watch) args.push('--watch');
   args.push('--config');
   args.push((process.env.NODE_ENV === 'production') ? prodConfig : devConfig);
   if (options.output) {
     args.push('--output');
-    args.push(output);
+    args.push(options.output);
   }
-  if (options.input) args.push(input);
-  var child = spawn('postcss', args, { stdio:  'inherit' })
+  if (options.input) {
+    args.push('--input');
+    args.push(options.input);
+  }
+  var child = spawn(postcssExec, args, { stdio:  ['inherit', 'inherit', 'pipe'] })
     , end = once(cb)
+    , error = null
   ;
+  child.stderr.on('data', function (data) {
+    if (!error) error = '';
+    error += data;
+  });
   child.on('error', end);
-  child.on('exit', function () { end(); });
+  child.on('exit', function () { end(error); });
 };
---
+++ test/api.js
@@ -0,0 +1,59 @@
+
+var cssnow = require('..')
+  , fs = require('fs')
+  , join = require('path').join
+  , assert = require('assert')
+  , shortid = require('shortid')
+  , simpleCSS = join(__dirname, 'fixtures/simple.css')
+  , brokenCSS = join(__dirname, 'fixtures/broken.css')
+  , delFiles = []
+;
+
+function run (src, cb) {
+  var file = join(__dirname, 'fixtures', shortid.generate());
+  delFiles.push(file);
+  cssnow({ input: src, output: file }, function (err) {
+    if (err) return cb(err);
+    cb(null, fs.existsSync(file) && fs.readFileSync(file, 'utf8'));
+  })
+}
+
+describe('API', function () {
+  this.timeout(10 * 1000);
+  after('clean up the files', function () {
+    delFiles.forEach(function (f) {
+      if (fs.existsSync(f)) fs.unlinkSync(f);
+    });
+  });
+  it('reports errors', function (done) {
+    run(brokenCSS, function (err, out) {
+      assert(err, 'there must be an error');
+      assert(!out, 'there must be no result');
+      done();
+    });
+  });
+  it('does not minify in development', function (done) {
+    process.env.NODE_ENV = 'development';
+    run(simpleCSS, function (err, out) {
+      assert.ifError(err);
+      assert(out.match(/(\n)/g).length > 7, 'plenty of newlines left');
+      done();
+    });
+  });
+  it('does minifies in production', function (done) {
+    process.env.NODE_ENV = 'production';
+    run(simpleCSS, function (err, out) {
+      assert.ifError(err);
+      assert.equal(out.match(/(\n)/g), null, 'minification worked');
+      done();
+    });
+  });
+  it('remains safe even under minification', function (done) {
+    process.env.NODE_ENV = 'production';
+    run(simpleCSS, function (err, out) {
+      assert.ifError(err);
+      assert(out.match(/80/), 'z-index wasn\'t killed');
+      done();
+    });
+  });
+});
